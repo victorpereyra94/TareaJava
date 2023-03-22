5ce5d605f5f2ffd47e0e572fe571f13c8c2d8b22
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-22 13:57:55 -0400

    Visitar la tumba del rey Alfred

---
+++ VisitarLaTumbaDelReyAlfred.java
@@ -0,0 +1,11 @@
+public class VisitarLaTumbaDelReyAlfred{
+        public static void main (String[] args){
+
+        System.out.println("Comprar vuelo a Inglaterra");
+        System.out.println("Preparar lo que llevare para el viaje");
+        System.out.println("Tomo el vuelo hacia Inglaterra");
+        System.out.println("Tomo el transporte hacia la tumba");
+}
+
+}
+
