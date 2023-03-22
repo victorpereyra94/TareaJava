1653b4b7d3cd19e2e0ffcc77045fc4a6e2d7621b
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-22 13:20:36 -0400

    Ir al trabajo

---
+++ IrAlTrabajo.java
@@ -0,0 +1,12 @@
+public class IrAlTrabajo{
+        public static void main(String[] args){
+
+        System.out.println("Me levanto en la mañana");
+        System.out.println("Me cepillo los dientes");
+        System.out.println("Me baño");
+        System.out.println("Me visto");
+        System.out.println("salgo de casa");
+        System.out.println("tomo el transporte");
+}
+
+}
