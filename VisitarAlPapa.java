f78f2b5b3d5c9fe908aa573130f5c4ad8e0e5747
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-22 14:20:59 -0400

    Visitar al papa

---
+++ VisitarAlPapa.java
@@ -0,0 +1,11 @@
+public class VisitarAlPapa{
+        public static void main(String[] args){
+
+        System.out.println("Comprar un vuelo a la ciudad del Vaticano");
+        System.out.println("Empacar mi equipaje para ir al aeropuerto");
+        System.out.println("Tomar el vuelo hacia la ciudad del Vaticano");
+        System.out.println("Ir al palacio del Vaticano y visitar al papa");
+        
+}
+
+}
