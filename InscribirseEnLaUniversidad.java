0c9159d87359a24561b5f3d54e79b03b1e0ec464
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-24 21:14:42 -0400

    Inscribirse en la universidad

---
+++ InscribirseEnLaUniversidad.java
@@ -0,0 +1,9 @@
+public class InscribirseEnLaUniversidad{
+        public static void main (String[] args){
+
+        System.out.println("Busco todos los documentos necesarios para inscribirme");
+        System.out.println("Lleno los documentos necesarios en la pagina de la universidad");
+        System.out.println("Pago la cuota de la inscripcion");
+}
+
+}
