ac2c5b556837c65e71a3ab0ea6a5344014d400ce
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-25 14:26:24 -0400

    Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora{
+        public static void main(String[] args){
+                int a = 48;
+                int b = 96;
+                System.out.println(sumar(a,b));
+
+}
+        public static int sumar(int a, int b){
+                return a + b;
+}
+
+}
