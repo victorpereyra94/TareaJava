d3e49d6a40ac07c74387f1431364c5f365c41764
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-24 21:02:03 -0400

    Comprar un pantalon usado

---
+++ ComprarUnPantalonUsado.java
@@ -0,0 +1,12 @@
+public class ComprarUnPantalonUsado{
+        public static void main (String[] args){
+
+        System.out.println("Tomo mi cartera");
+        System.out.println("Tomo el transporte");
+        System.out.println("Voy a la tienda");
+        System.out.println("Veo los pantalones que tienen");
+        System.out.println("Me mido el pantalon en el vestidor para ver si me sirve");
+        System.out.println("Procedo a pagar el pantalon");
+}
+
+}
