1d3389082ee26a918f93d94471e44fa7b7caf294
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-24 21:36:55 -0400

    Dejar de tomar alcohol

---
+++ DejarDeTomarAlcohol.java
@@ -0,0 +1,9 @@
+public class DejarDeTomarAlcohol{
+        public static void main (String[] args){
+
+        System.out.println("Decidir dejar de tomar alcohol");
+        System.out.println("Inscribirme en alcoholicos anonimos para recibir ayuda con relacion a eso");
+        System.out.println("Evitar las bebidas que contengan alcohol");
+}
+
+}
