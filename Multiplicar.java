778f8144cd8442c6db116008fbef0b449465748d
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-25 14:16:09 -0400

    Multiplicar

---
+++ Multiplicar.java
@@ -0,0 +1,11 @@
+public class Multiplicar{
+        public static void main(String[] args){
+               int c = 47;
+               int z = 6;
+               System.out.println(multiplicar(c,z));
+}       
+        public static int multiplicar(int c, int z){
+               return c * z;        
+}
+
+}
