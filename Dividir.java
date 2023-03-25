5bb7901a09bf61dbcaf0d3cafdfe5b3504fc7787
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-25 14:27:19 -0400

    Dividir

---
+++ Dividir.java
@@ -0,0 +1,12 @@
+public class Dividir{
+       public static void main(String[] args){
+              int f = 97;
+              int d = 4;
+              System.out.println(dividir(f,d));
+
+}
+       public static int dividir(int f, int d){
+              return f / d;
+}
+
+}
