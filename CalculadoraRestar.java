53202c90f2b750016d5d709787c49a981ec20126
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-25 13:50:38 -0400

    Calculadora restar

---
+++ CalculadoraRestar.java
@@ -0,0 +1,12 @@
+public class CalculadoraRestar{
+        public static void main(String[] args){
+               int x = 87;
+               int y = 49;
+               System.out.println(restar(x,y));
+
+}
+        public static int restar(int x, int y){
+               return x - y;
+}
+
+}
