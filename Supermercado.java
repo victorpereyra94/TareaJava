ac539294d337f81dbdebf253c721dd6e136a4c1e
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-28 22:50:56 -0400

    Supermercado

---
+++ Supermercado.java
@@ -0,0 +1,16 @@
+/*Maria ira al supermercado si tiene mas de 100 dolares, sino, maria nor ira al supermercado*/
+
+public class Supermercado{
+       public static void main(String[] args){
+              int presupuestoMaria = 98;
+              if(presupuestoMaria == 100){
+                          System.out.println("Maria ira al supermercado");
+       
+}              else{
+                          System.out.println("Maria no ira al supermercado");
+
+}
+  
+}
+
+}
