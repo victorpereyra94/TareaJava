951e8968bdc72631d4e2cdd41de9e4952da14250
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-28 23:10:30 -0400

    Semafaro

---
+++ Semafaro.java
@@ -0,0 +1,15 @@
+/*Jose puede seguir conduciendo si la luz del semafaro es verde, sino, jose debe detenerse*/
+
+public class Semafaro{
+       public static void main(String[] args){
+              boolean josePuedeSeguirConduciendo = true;
+              if(josePuedeSeguirConduciendo){
+                     System.out.println("si la luz del semafaro es verde");
+
+}             else{
+                     System.out.println("debe detenerse");
+}
+
+}
+
+}
