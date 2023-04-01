895aa25523d8f2e23eeffbb41e14da24647abe9f
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-31 23:33:30 -0400

    Menu

---
+++ Menu.java
@@ -0,0 +1,15 @@
+/*Mostrar el menu si son las 11, sino, solo mostar bebidas*/
+
+public class Menu{
+       public static void main(String[] args){
+              boolean mostrarElMenu = false;
+              if(mostrarElMenu){
+                      System.out.println("si son las 11");
+
+} 
+           else{
+}                     System.out.println("solo mostrar bebidas");
+
+}
+
+}
