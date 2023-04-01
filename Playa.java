f61f94a54faa59f8066bbbf0db7743b482ea98e2
victorpereyra94 <127797859+victorpereyra94@users.noreply.github.com>
2023-03-31 23:13:30 -0400

    Playa

---
+++ Playa.java
@@ -0,0 +1,15 @@
+/*Puedes ir a la playa si tienes mas de 18 años, sino, debes ir a estudiar*/
+
+public class Playa{
+       public static void main(String[] args){
+              boolean puedeIrAlaPlaya = false;
+              if(puedeIrAlaPlaya){
+                      System.out.println("si tiene 18 años");
+
+
+}             else{
+}                     System.out.println("debe ir a estudiar");            
+
+}
+
+}
